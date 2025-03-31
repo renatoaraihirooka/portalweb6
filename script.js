5d84817871b6d5a00128ea2df326c66954244794
Renato Arai <renatoarai@gmail.com>
2025-03-30 23:23:00 -0300

    teste

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const owner = "renatoaraihirooka"; // Substitua pelo usuário ou organização
 const repo = "portal2"; // Substitua pelo nome do repositório
-const githubToken = `${{GITHUB_TOKEN}}`; // Substitua pelo seu token pessoal
+const githubToken = '${{ secrets.GITHUB_TOKEN }}'; // Substitua pelo seu token pessoal
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('runTests').addEventListener('click', async () => {
